4e08a07ff5cde7e7b9b7e6d6d51c28d4fcf17a83
lonely-caat <maxbilichenko@gmail.com>
2023-05-16 16:43:50 +0300

    Using map but not using the interface

--- cards/src/App.tsx
+++ cards/src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 
+enum CardSuit {
+    Spades = "♠",
+    Clubs = "♣",
+    Hearts = "♥",
+    Diamonds = "♦",
+}
+
 interface Card {
-    suit: string;
+    suit: CardSuit;
     rank: string;
 }
 
-
-const cardSuits: string[] = ["♠", "♣", "♥", "♦"];
+const cardSuits: CardSuit[] = [CardSuit.Spades, CardSuit.Clubs, CardSuit.Hearts, CardSuit.Diamonds];
 const cardRanks: string[] = [
     "2",
     "3",
@@ -24,20 +30,15 @@ const cardRanks: string[] = [
 ];
 
 export default function Card(){
-    const cardArray = []
-    for (const suit of cardSuits){for (const rank of cardRanks){
-        const card: Card = {suit, rank};
-        cardArray.push(card);
-    }}
-
-    const cards = cardArray.map((card) => `${card.rank} ${card.suit}`);
-    console.log(cards)
+    const cardArray = cardSuits.map((suit) =>
+        cardRanks.map((rank) => suit + rank)
+    );
 
     return (
         <div>
             <h1>Deck of Cards</h1>
             {/*TODO: how do i make this an ordered list? with each element on a new line?*/}
-            <p>{cards}</p>
+            <p>{cardArray}</p>
         </div>
     );
 
