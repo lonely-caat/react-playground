d669b463d1c24f69042d6e4cec00d2ed996d7472
lonely-caat <maxbilichenko@gmail.com>
2023-05-16 16:52:47 +0300

    adding both map and type

--- cards/src/App.tsx
+++ cards/src/App.tsx
@@ -23,15 +23,13 @@ const cardRanks: string[] = [
     "A",
 ];
 
-export default function Card(){
-    const cardArray = []
-    for (const suit of cardSuits){for (const rank of cardRanks){
-        const card: Card = {suit, rank};
-        cardArray.push(card);
-    }}
+export default function Card() {
+    const cardArray: Card[] = cardSuits.map((suit) =>
+        cardRanks.map((rank) => ({ suit, rank }))
+    ).flat();
 
     const cards = cardArray.map((card) => `${card.rank} ${card.suit}`);
-    console.log(cards)
+    console.log(cards);
 
     return (
         <div>
